f4101e73bf03ffd9b589217600b5c3f54c66447b
= <=>
2019-12-13 22:24:22 -0500

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-    
+    validates :user, presence: true, 
+    uniqueness: {case_sensitive: false}, 
+    length: {minimum:3, maximum:20}
+
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,
+    uniqueness: {case_sensitive: false},
+    format: {with: EMAIL_REGEX}
+
 end
