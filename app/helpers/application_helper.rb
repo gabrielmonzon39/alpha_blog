e4120aa79b7102289bc345882b7660a2ae6c6f26
= <=>
2019-12-14 18:25:49 -0500

    Index for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     def gravatar_for user
         gravatar_id = Digest::MD5::hexdigest(user.email)
-        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=150"
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=100"
         image_tag(gravatar_url, alt:"User Avatar", class: "img-thumbnail")
     end
 end
