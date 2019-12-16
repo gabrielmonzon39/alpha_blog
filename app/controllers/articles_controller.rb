3674c8a9e9e62282a226a34127cfd2ee8ecd88fd
= <=>
2019-12-16 15:13:37 -0500

    show and edit categories, update view for categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
 
     private
         def get_params
-            params.require(:article).permit(:title, :description)
+            params.require(:article).permit(:title, :description, category_ids:[])
         end
 
         def find_article
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,6 +11,8 @@ class CategoriesController < ApplicationController
     end
 
     def show
+        @category = Category.find(params[:id])
+        @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def create
@@ -22,6 +24,21 @@ class CategoriesController < ApplicationController
             render 'new'
         end    
     end
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:success] = "Category updated"
+            redirect_to category_path(@category)
+        else
+            render 'edit'
+        end   
+    end
+    
    
     private
     def category_params
