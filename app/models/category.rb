54eb86083de46c102350524a073bb40dda4cd49b
= <=>
2019-12-16 13:16:35 -0500

    many to many associaton, categories and articles

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ApplicationRecord
 
+    has_many :article_categories
+    has_many :articles, through: :article_categories
+
     validates :name, presence: true, 
     uniqueness: {case_sensitive: false}, 
     length: {minimum:3, maximum:20}
